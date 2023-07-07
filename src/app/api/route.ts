d01adcd6ed8c371f14ff1bfc035a42d33d42aa63
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2023-07-07 21:41:00 +0530

    some basic changes to language

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request, res: NextResponse) {
     const body = await req.json()
   
     const completion = await openai.createChatCompletion({
-      model:"text-davinci-003",
+      model:"gpt-3.5-turbo",
       messages: body.messages,
     });
     console.log(completion.data.choices[0].message);
