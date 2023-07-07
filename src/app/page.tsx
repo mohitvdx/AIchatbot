d6d0174a7977d4f90f9803e9968fd867bdbfb5e3
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2023-07-07 13:25:44 +0530

    removed a jsx function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-export default function Home() {
+
   const [theInput, setTheInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState([
